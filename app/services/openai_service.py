8c5378a5b9707aaabec075eee933391cfe86f3f1
Caro. Z. <czumbiehlfaure@hotmail.fr>
2025-03-24 17:15:19 +0000

    V13

---
+++ app/services/openai_service.py
@@ -0,0 +1,16 @@
+# pour isoler la logique liée à OpenAI et pouvoir changer de  modèle plus facilement
+import os
+import openai
+
+def ask_openai(question: str) -> str:
+    openai.api_key = os.getenv("OPENAI_API_KEY", "fake-key-for-tests")
+
+    response = openai.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": "Tu es un assistant qui aide à comprendre une docum>
+            {"role": "user", "content": question}
+        ]
+    )
+
+    return response.choices[0].message.content
